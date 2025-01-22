2e31046796e990f29cd8188b2a9e429ffd1af1de
Running water <alsgks1218@naver.com>
2025-01-22 10:54:12 +0900

    레이아웃 추가

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">Aout Us</h1>
+      <div className="mt-5">소개 페이지와 힘내고 멋진 하루</div>
+    </main>
+  );
+}
