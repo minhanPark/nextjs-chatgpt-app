037cdeaeedbb6f9cdeae73f6542471d02ef157f8
Running water <alsgks1218@naver.com>
2025-02-01 17:08:00 +0900

    add auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Link from "next/link";
-
+import { SessionProvider } from "./components/SessionProvider";
 import "./globals.css";
+import UserButton from "./components/UserButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,23 +26,27 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased px-2 md:px-5`}
-      >
-        <header className="text-white font-bold bg-green-900 text-2xl p-2 mb-3 rounded-b-lg shadow-gray-700 shadow-lg flex">
-          <div className="flex flex-grow">
-            <Link href="/">GPT Chat</Link>
-            <Link href="/about" className="ml-5 font-light">
-              About
-            </Link>
+    <SessionProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased px-2 md:px-5`}
+        >
+          <header className="text-white font-bold bg-green-900 text-2xl p-2 mb-3 rounded-b-lg shadow-gray-700 shadow-lg flex">
+            <div className="flex flex-grow">
+              <Link href="/">GPT Chat</Link>
+              <Link href="/about" className="ml-5 font-light">
+                About
+              </Link>
+            </div>
+            <div className="">
+              <UserButton />
+            </div>
+          </header>
+          <div className="flex flex-col md:flex-row">
+            <div className="flex-grow">{children}</div>
           </div>
-          <div className=""></div>
-        </header>
-        <div className="flex flex-col md:flex-row">
-          <div className="flex-grow">{children}</div>
-        </div>
-      </body>
-    </html>
+        </body>
+      </html>
+    </SessionProvider>
   );
 }
