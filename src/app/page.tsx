8bec08446d94a14398196ba987005ba2567984bf
Running water <alsgks1218@naver.com>
2025-02-05 15:07:24 +0900

    카드 등록은 안해서 작동안됨

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <h1>Welcome to gpt app</h1>;
+import { Separator } from "@/components/ui/separator";
+import Chat from "./components/Chat";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession();
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">Welcome to GPT Chat</h1>
+      {!session?.user?.email && <div>you need to log in to use this chat</div>}
+      {session?.user?.email && (
+        <>
+          <Chat />
+          <Separator />
+        </>
+      )}
+    </main>
+  );
 }
---
+++ src/app/server-actions/getCompletion.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPEN_AI_KEY,
+});
+
+export async function getCompletion(
+  messageHistory: {
+    role: "user" | "assistant";
+    content: string;
+  }[]
+) {
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: messageHistory,
+  });
+
+  const messages = [
+    ...messageHistory,
+    response.choices[0].message as unknown as {
+      role: "user" | "assistant";
+      content: string;
+    },
+  ];
+
+  return { messages };
+}
